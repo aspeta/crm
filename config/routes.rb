efb4f05e4b0399a9c65fd46b1e2a63cc40819f73
Aspet Amirkhanian <aspeta@yahoo.com>
2015-01-14 05:29:18 +0000

    Installed bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Crm::Application.routes.draw do
+  get "welcome/index"
+  get "welcome/customer"
+ 
+root to: 'welcome#customer'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
